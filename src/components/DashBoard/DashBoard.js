9ddf069fd407f4748117beba76c3b79fc6ac7a0b
Volodya <volodyaserobyan732@gmail.com>
2019-12-23 05:10:59 -0800

    Changing sizes of Login page

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import PublicLayout from '../PublicLayout/PublicLayout'
 
 const DashBoard = () => {
 
     return (
         <section className="Dashboard">
-            
+            <PublicLayout />
         </section>
     )
 }
--- src/components/PublicLayout/PublicLayout.js
+++ src/components/PublicLayout/PublicLayout.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import dolarIcon from '../../assets/vectorDolar.png'
+import dolarIcon from '../../assets/VectorDolar.png'
 import ipTv from '../../assets/iptv3.png'
 import dashIcon from '../../assets/Group 19.png'
 import tvIcon from '../../assets/VectorTv.png'
 import userIcon from '../../assets/VectorUser.png'
-import addUserIcon from '../../assets.VectoraddUser.png'
+import addUserIcon from '../../assets/VectoraddUser.png'
 import settingsIcon from '../../assets/Settings.png'
 import './PublicLayout.scss'
 
-const PublicLayout = ({children}) => {
+const PublicLayout = ({ children }) => {
 
     return (
         <section>
@@ -20,20 +20,19 @@ const PublicLayout = ({children}) => {
                 </div>
                 <div className="Nav-Wrapper">
                     <ul>
-                        <li><img src={dashIcon} /></li>
-                        <li><img src={tvIcon} /></li>
-                        <li><img src={userIcon} /></li>
-                        <li><img src={addUserIcon} /></li>
-                        <li><img src={settingsIcon} /></li>
+                        <li><img src={dashIcon} alt="" /></li>
+                        <li><img src={tvIcon} alt="" /></li>
+                        <li><img src={userIcon} alt="" /></li>
+                        <li><img src={addUserIcon} alt="" /></li>
+                        <li><img src={settingsIcon} alt="" /></li>
                     </ul>
                 </div>
             </aside>
-            <nav className="Navbar">
-                <div className="Navbar-Balance">
+            <div>
+                <nav className="Navbar">
                     <img src={dolarIcon} alt="" />
-                    <p></p>
-                </div>
-            </nav>
+                </nav>
+            </div>
         </section>
     )
 }
